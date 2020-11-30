c75f09e1800756bd68ed03610eb21ecc1912ecec
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-11-30 19:33:23 +0530

    [Arti Dandge] Add : Check Employee is present or not

--- EmployeeWageCalculation/Program.cs
+++ EmployeeWageCalculation/Program.cs
@@ -7,6 +7,20 @@ namespace EmployeeWageCalculation
         static void Main(string[] args)
         {
             Console.WriteLine("Welcom to Employee Wage Calculation Project !");
+            //Constants
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if(empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else 
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
