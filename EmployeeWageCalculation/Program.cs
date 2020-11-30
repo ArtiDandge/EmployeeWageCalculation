860318b4be0c045e1bd1795b75e2a5eec430e63b
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-11-30 20:15:33 +0530

    [Arti Dandge] Refactor : Check Employee is full time or part time or absent using switch case intead if-else statement

--- EmployeeWageCalculation/Program.cs
+++ EmployeeWageCalculation/Program.cs
@@ -4,32 +4,30 @@ namespace EmployeeWageCalculation
 {
     class Program
     {
+        //Constants
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("Welcom to Employee Wage Calculation Project !");
-            //Constants
-            int IS_PART_TIME = 1;
-            int IS_FULL_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
-            Random random = new Random();
-
             //Variables
             int empHrs = 0;
             int empWage = 0;
-
+            Random random = new Random();
             //Computation
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
+            switch (empCheck)
             {
-                empHrs = 4;
-            }
-            else if(empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else 
-            {
-                empHrs = 0;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage : " + empWage);
