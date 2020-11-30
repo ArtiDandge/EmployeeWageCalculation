9a0caa0c8f05a26fc72342f3a1be77bce23fa361
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-11-30 19:53:00 +0530

    [Arti Dandge] Add : Calculate Employee Wage

--- EmployeeWageCalculation/Program.cs
+++ EmployeeWageCalculation/Program.cs
@@ -9,18 +9,25 @@ namespace EmployeeWageCalculation
             Console.WriteLine("Welcom to Employee Wage Calculation Project !");
             //Constants
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
             Random random = new Random();
+
+            //Variables
+            int empHrs = 0;
+            int empWage = 0;
 
             //Computation
             int empCheck = random.Next(0, 2);
             if(empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else 
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
         }
     }
 }
